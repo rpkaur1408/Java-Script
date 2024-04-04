af7aaf8c8ef1956ad1cd5b4e0fc3bab7f33cbed0
rpkaur1408 <rehatpreetkaur981@gmail.com>
2024-04-04 12:19:31 -0400

    Commiting Loops and Variables

---
+++ Operators/operator.js
@@ -0,0 +1,21 @@
+let a = 5;
+let b = '5';
+
+console.log("a==b",a==b); //true 
+console.log("a===b",a===b); // false
+
+alert("hello!");  // pop-up
+prompt("hello!"); // will take input as 
+
+// to get input
+
+let name = prompt ("hello!");
+console.log(name);
+
+let x= prompt ("Enter a number : ");
+if (x%5==0){
+    console.log(x+"  is a multiple of 5");
+}
+else{
+    console.log(x+ " is not a multiple of 5");
+}
---
+++ loops/loop.js
@@ -0,0 +1,42 @@
+// FOR - OF LOOP
+let str ="JavaScript";
+
+let size =0;
+for (let val of str){
+    console.log("val = ",val);
+    size++;
+}
+
+console.log("String size = ",size);
+
+// FOR - IN LOOP
+let student ={
+    name : "divyanshu dugar",
+    age : 20,
+    cpgpa :7.5,
+    isPass: true,
+
+};
+
+for (let key in student){
+    console.log("key = ",key," value = ",student[key]);
+}
+
+//Strings
+let str1 ="Rehatpreet kaur";  //Method 1
+let str2 ='Divyanshu Dugar';  // Method 2
+
+//Template literals
+
+let specialString =`This is a template literal`;
+
+let obj ={ 
+    item :'pen',
+    price :10,
+
+};
+
+console.log(`The cost of the ${obj.item} is ${obj.price}`);
+
+
+
